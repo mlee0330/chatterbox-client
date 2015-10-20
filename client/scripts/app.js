1d202b9b7a09aa9421861b4380294cd06ba19d23
mlee0330 <michaelyurilee@gmail.com>
2015-10-20 16:15:58 -0700

    Implemented basic requirements

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,16 +1,17 @@
 var username = window.location.search.split('=')[1];
-var rooms = {};
+var roomFilter = '';
+var updateRoomList = true;
+var friends = {};
 
 var message = {
   username: username,
   text: '<script>alert(\'hi\')</script>',
-  roomname: 'hr10'
+  roomname: 'testingroom'
 };
 
 
-
 var sanitize = function(str) {
-  if(str === undefined) {
+  if(str === undefined || str === null) {
     return;
   }
   var stringArr = str.split('');
@@ -28,8 +29,10 @@ var sanitize = function(str) {
 };
 
 var messageParser = function(data) {
+  var rooms = {};
   var results = data.results;
   var $feed = $(".feed");
+  $feed.empty();
   console.log(data);
   for( var i = 0; i < results.length; i++) {
     var message = sanitize(results[i].text);
@@ -38,14 +41,23 @@ var messageParser = function(data) {
     if (results[i].roomname !== undefined){
       rooms[results[i].roomname] = results[i].roomname;
     }
-    console.log(message);
-    $feed.append("<p>"+username+": "+message+"</p>");
+    //console.log(message);
+    var friendClass = '';
+    if (friends[username] !== undefined){
+      friendClass = 'friend';
+    }
+    $feed.append("<div><span class='username " + friendClass +"'>"+username+"</span>: <span class='message " + friendClass + " '>"+message+"</span></div>");
+  }
+  if(updateRoomList) {
+    updateRoomDropdown(rooms);
   }
 };
 
+// url + order + where
+var baseURL = 'https://api.parse.com/1/classes/chatterbox?order=-createdAt'; 
 var getMessages = {
   // This is the url you should use to communicate with the parse API server.
-  url: 'https://api.parse.com/1/classes/chatterbox?order=-createdAt',
+  url: baseURL,
   type: 'GET',
   contentType: 'application/json',
   success: messageParser,
@@ -70,23 +82,77 @@ var sendMessage = {
   }
 };
 
+var submitMessage = function(){
+  message.text = $('.message').val();
+  sendMessage.data = JSON.stringify(message);
+  //set updateRoomList to false so room menu doesn't clear when running getMessages
+  updateRoomList = false;
+  $.ajax(sendMessage).success(function(){
+    $.ajax(getMessages).success(function(){
+      updateRoomList = true;
+    });
+  });
+  $('.message').val("");
+};
+
+var updateRoomDropdown = function(rooms){
+  $('.dropdown-menu').empty();
+  $('.dropdown-menu').append('<li class ="drop-link">All Rooms</li>');
+  $('.dropdown-menu').append('<li role="separator" class="divider"></li>');
+  for (var room in rooms){
+    //append li elements for each room
+    $('.dropdown-menu').append('<li class="drop-link">'+ room +'</li>');
+  }
+};
+
+var selectRoom = function(room){
+  roomFilter = '';
+  if (room !== "All Rooms"){
+    roomFilter = '&where={"roomname":' + '"' + room + '"' + '}';
+  }
+  message.roomname = room;
+  getMessages.url = baseURL + roomFilter;
+  $.ajax(getMessages).success(function(){
+    updateRoomList = true;
+  });
+};
 
 //$.ajax(sendMessage);
 $.ajax(getMessages);
 
 $(document).ready(function() {
+  
   $('.update-btn').click(function(){
-    $('.feed p').remove();
     $.ajax(getMessages);
   });
 
   $('.send-btn').click(function(){
-    message.text = $('.message').val();
-    sendMessage.data = JSON.stringify(message);
-    $.ajax(sendMessage);
-    $('.feed p').remove();
-    $.ajax(getMessages);
-    $('.message').val("");
+    submitMessage();
   });
 
+  $('.message').bind('keypress', function(e){
+    if (e.which === 13) { 
+      event.preventDefault();
+      submitMessage();
+    }
   });
+
+  $(document).on("click", '.drop-link', function() {
+    console.log($(this).text());
+    $(".dropdown-toggle").text($(this).text());
+    updateRoomList = false;
+    selectRoom($(this).text());
+  });
+
+  $(document).on("click", '.username', function() {
+    console.log($(this).text());
+    var $friend = $(this).text();
+    friends[$friend] = $friend;
+    updateRoomList = false;
+    $.ajax(getMessages).success(function(){
+      updateRoomList = true;
+    });
+  });
+
+
+});
